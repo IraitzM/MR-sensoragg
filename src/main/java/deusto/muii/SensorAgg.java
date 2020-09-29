481749c52e8700265b43b8339bdef70436f0144b
IraitzM <iraitzm@gmail.com>
2020-09-29 09:58:38 +0200

    Added main code

---
+++ src/main/java/deusto/muii/SensorAgg.java
@@ -0,0 +1,69 @@
+package deusto.muii;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class SensorAgg {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable>{
+
+        final String DELIMITER = ",";
+        private final static IntWritable val = new IntWritable();
+        private Text sensor = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) {
+            try {
+                String valueString = value.toString();
+                String[] data = valueString.split(DELIMITER);
+                sensor.set(data[2]);
+                val.set(Integer.parseInt(data[5]));
+                context.write(sensor, val);
+            }catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Sensor Agg");
+        job.setJarByClass(SensorAgg.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
